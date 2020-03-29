7b80a9a91df7e8a59050c9dad2a951a3ec50e77f
anthonybartczak <to0oni456@gmail.com>
2020-03-29 13:05:14 +0200

    Cell neighbour scheme

--- gameoflife.py
+++ gameoflife.py
@@ -21,12 +21,35 @@ def updateGrid():
 
 def main():
 
+    # Neighbour system
+    # 1 2 3
+    # 4 o 6
+    # 7 8 9
+    #
+    # 1 -> array[x - 1][y - 1]
+    # 2 -> array[x - 1][y]
+    # 3 -> array[x - 1][y + 1]
+    # 4 -> array[x][y - 1]
+    # 6 -> array[x][y + 1]
+    # 7 -> array[x + 1][y - 1]
+    # 8 -> array[x + 1][y]
+    # 8 -> array[x + 1][y + 1]
+
+
+    # Elements are referenced [row][column] with starting index 0
     array = defineArray(row, col)
-    newArray = defineArray(row, col)
 
     plot.imshow(array, interpolation='nearest')
     plot.gray()
     plot.show()
+
+    # Create a new grid system based on the last one
+    newArray = defineArray(row, col)
+
+    # Get the count of alive neighbour cells
+    #for x in range(row):
+        #for y in range(col):
+
     
 
 if __name__ == '__main__': 
