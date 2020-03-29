940bac2a8144b98ed03b5a9de6dc60b741a4f9b3
anthonybartczak <to0oni456@gmail.com>
2020-03-29 14:54:40 +0200

    Animations added

--- gameoflife.py
+++ gameoflife.py
@@ -2,6 +2,7 @@ from threading import Thread
 from numpy import zeros
 from random import randrange
 from matplotlib import pyplot as plot
+from matplotlib import animation
 
 width, height = 600, 400
 dotres = 20
@@ -16,12 +17,41 @@ def defineArray(row, col):
             array[x][y] = randrange(0,2)
     return array
 
-def updateGrid():
-    pass
+def neighbourCellCounter(array, xo, yo):
+    # xo, yo - center cell position
+    # Go around the neighbour cells
+    # Count the alive ones
+
+    aliveSum = 0
+    for x in range(0,2):
+        for y in range(0,2):
+            print (xo + x, yo + y)
+            aliveSum += array[xo + x][yo + y]
+    aliveSum -= array[xo][yo]
+    return aliveSum
+
+def updateGrid(ogScene):
+    # Create a new grid system based on the last one
+    nextScene = defineArray(row, col)
+    print(range(row))
+    print(range(col))
+    for x in range(row):
+        for y in range(col):
+            cellState = ogScene[x][y]
+            if x == 0 or x == col - 1 or y == 0 or y == row - 1:
+                nextScene[x][y] = cellState
+                continue
+        nSum = neighbourCellCounter(ogScene, x, y)
+        
+        if cellState == 0 and nSum == 3:
+            nextScene[x][y] = 1
+        elif cellState == 1 and (nSum < 2 or nSum > 3):
+            nextScene[x][y] = ogScene[x][y]
+    plot.imshow(nextScene, interpolation='nearest')
 
 def main():
 
-    # Neighbour system
+    # Neighbour cells system
     # 1 2 3
     # 4 o 6
     # 7 8 9
@@ -37,20 +67,14 @@ def main():
 
 
     # Elements are referenced [row][column] with starting index 0
-    array = defineArray(row, col)
+    ogScene = defineArray(row, col)
 
-    plot.imshow(array, interpolation='nearest')
+    fig, ax = plot.subplots()
+
+    #
+    ani = animation.FuncAnimation(fig, updateGrid(ogScene), interval=2, blit=True, save_count=50)
     plot.gray()
     plot.show()
-
-    # Create a new grid system based on the last one
-    newArray = defineArray(row, col)
-
-    # Get the count of alive neighbour cells
-    #for x in range(row):
-        #for y in range(col):
-
-    
 
 if __name__ == '__main__': 
     main() 
